0c9059d5af32090c8fb452a58dca29d88816710b
PabloCarmonaGuerrero1 <pcargue233@g.educaand.es>
2023-09-23 17:56:58 +0200

    Completo

--- JS/JavaScript_Externo.js
+++
@@ -1,8 +0,0 @@
-function handleClick() {
-    alert("Bien hecho!");
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    var button = document.getElementById("b2");
-    button.addEventListener("click", handleClick);
-});
